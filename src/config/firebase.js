6df75824a5b1b16b7f5a445c0e283a001d3b6436
Manuel Solís Gómez <msolisz@alumnos.unex.es>
2025-12-02 06:57:39 +0100

    fixed/Eliminar errores de fiabilidad nuevo

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -31,7 +31,7 @@ if (!admin.apps.length) {
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replaceAll(/\\n/g, '\n'),
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replaceAll('\\n', '\n'),
     }),
   });
 }
